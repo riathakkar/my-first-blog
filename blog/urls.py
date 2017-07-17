4abbd876917e3d57bc98809f9eef42208f5e1375
riathakkar <riathakkar16@gmail.com>
2017-07-16 20:07:29 -0700

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^aboutme$', 'aboutme', name='aboutme'),
+    url(r'^aboutme/$', 'aboutme', name='aboutme'),
 ]
